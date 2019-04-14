e9718e7fe15d9f7cd8b983023f50112612f46d22
ChicoLinux <44916083+Oreramirez@users.noreply.github.com>
2019-04-13 22:49:47 -0500

    Informe01
    Informe de laboraotorio01

--- labo/labo/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace labo.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- labo/labo/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace labo.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
